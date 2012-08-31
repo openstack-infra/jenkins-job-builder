0c2bef08030f6b950e6d6ff65f8db3764d23490f
James E. Blair <jeblair@hp.com>
2012-08-31 15:15:39 -0700

    Add test to determine whether XML output has changed.
    Change-Id: I5e0b76e195a1b412d180484ce1ddd3e9159d7779
    Reviewed-on: https://review.openstack.org/12286
    Approved: James E. Blair <corvus@inaugust.com>
    Reviewed-by: James E. Blair <corvus@inaugust.com>
    Tested-by: Jenkins

---
+++ tools/run-compare-xml.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+
+# Copyright (c) 2012, AT&T Labs, Yun Mao <yunmao@gmail.com>
+# All Rights Reserved.
+# Copyright 2012 Hewlett-Packard Development Company, L.P.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+rm -fr .test
+mkdir -p .test/old
+mkdir -p .test/new
+cd .test
+git clone https://review.openstack.org/p/openstack/openstack-ci-puppet --depth 1
+cd ..
+GITHEAD=`git rev-parse HEAD`
+
+# First generate output from HEAD~1
+git checkout HEAD~1
+tox -e compare-xml-old
+
+# Then use that as a reference to compare against HEAD
+git checkout $GITHEAD
+tox -e compare-xml-new
+
+CHANGED=0
+for x in `(cd .test/old && find -type f)`
+do
+    if ! diff -u .test/old/$x .test/new/$x >/dev/null 2>&1
+    then
+	CHANGED=1
+	echo "============================================================"
+	echo $x
+	echo "------------------------------------------------------------"
+    fi
+    diff -u .test/old/$x .test/new/$x
+done
+
+echo
+echo "You are in detached HEAD mode. If you are a developer"
+echo "and not very familiar with git, you might want to do"
+echo "'git checkout branch-name' to go back to your branch."
+
+if [ "$CHANGED" -eq "1" ]; then
+    exit 1
+fi
+exit 0
