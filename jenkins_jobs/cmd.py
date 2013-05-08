73540705e1253f8b0537d67ca406ed7e5b85b17d
James E. Blair <jeblair@openstack.org>
2013-05-08 14:02:33 -0700

    Make it easier to run in place.
    Without installing.
    
    Change-Id: I63f322178aacef51b559cc04899d8dec7c67a219
    Reviewed-on: https://review.openstack.org/28613
    Reviewed-by: Khai Do <zaro0508@gmail.com>
    Reviewed-by: Monty Taylor <mordred@inaugust.com>
    Reviewed-by: Antoine Musso <hashar@free.fr>
    Approved: Clark Boylan <clark.boylan@gmail.com>
    Reviewed-by: Clark Boylan <clark.boylan@gmail.com>
    Tested-by: Jenkins

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+# Copyright (C) 2012 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-import jenkins_jobs.builder
-import jenkins_jobs.errors
 import argparse
 import ConfigParser
 import logging
@@ -16,6 +27,8 @@ def confirm(question):
 
 
 def main():
+    import jenkins_jobs.builder
+    import jenkins_jobs.errors
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers(help='update, test or delete job',
                                       dest='command')
@@ -89,4 +102,5 @@ def main():
                            output_dir=options.output_dir)
 
 if __name__ == '__main__':
+    sys.path.insert(0, '.')
     main()
