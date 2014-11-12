288af951b536bd93c820171306e86d1c12b83a51
Wayne <wayne@puppetlabs.com>
2014-11-11 23:50:45 -0800

    Reorganize tests/cmd
    This commit reorganizes the 'cmd' tests by subcommand.
    
    Previously the multipath test was using an actual fixture directory
    which actually made the fixtures directory somewhat unusable by other
    tests since with an upcoming change (get_plugins_version) fixtures
    directories may also contain YAML that doesn't contain JJB data.
    
    Change-Id: I4c3866e1e0769512b40e1790e40affedb13953a2

---
+++ tests/cmd/subcommands/test_delete.py
@@ -0,0 +1,45 @@
+import os
+import mock
+from jenkins_jobs import cmd
+from tests.cmd.test_cmd import CmdTestsBase
+
+
+class DeleteTests(CmdTestsBase):
+
+    @mock.patch('jenkins_jobs.cmd.Builder.delete_job')
+    def test_delete_single_job(self, delete_job_mock):
+        """
+        Test handling the deletion of a single Jenkins job.
+        """
+
+        args = self.parser.parse_args(['delete', 'test_job'])
+        cmd.execute(args, self.config)  # passes if executed without error
+
+    @mock.patch('jenkins_jobs.cmd.Builder.delete_job')
+    def test_delete_multiple_jobs(self, delete_job_mock):
+        """
+        Test handling the deletion of multiple Jenkins jobs.
+        """
+
+        args = self.parser.parse_args(['delete', 'test_job1', 'test_job2'])
+        cmd.execute(args, self.config)  # passes if executed without error
+
+    @mock.patch('jenkins_jobs.builder.Jenkins.delete_job')
+    def test_delete_using_glob_params(self, delete_job_mock):
+        """
+        Test handling the deletion of multiple Jenkins jobs using the glob
+        parameters feature.
+        """
+
+        args = self.parser.parse_args(['delete',
+                                       '--path',
+                                       os.path.join(self.fixtures_path,
+                                                    'cmd-002.yaml'),
+                                       '*bar*'])
+        cmd.execute(args, self.config)
+        calls = [mock.call('bar001'), mock.call('bar002')]
+        delete_job_mock.assert_has_calls(calls, any_order=True)
+        self.assertEquals(delete_job_mock.call_count, len(calls),
+                          "Jenkins.delete_job() was called '%s' times when "
+                          "expected '%s'" % (delete_job_mock.call_count,
+                                             len(calls)))
