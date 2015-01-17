d7c37c89a810174afb05499a6a0142384cd1bf21
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-01-17 11:45:44 -0500

    Update SCM Wipe workspace to use extensions style
    Use the newer extensions style configuration for the SCM Wipe workspace
    option. This change simply changes the output style of this
    configuration leaving the original YAML configuration values in tact so
    it is backwards compatible with the old style configuration for
    wipe-workspace setting.
    
    While this patch still sets the old <wipeOutWorkspace> value long with
    the new extensions value. Jenkins appears to be happy with both values
    present and prefers the extensions format if it exists. This should
    allow the older Jenkins that only supports the older config format to
    continue to work.
    
    Change-Id: Ib5b1809d19a35f83f524ee84b18f0c58b851c70b
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins_jobs/modules/scm.py
+++ jenkins_jobs/modules/scm.py
@@ -89,7 +89,6 @@ remoteName/\*')
     :arg str reference-repo: Path of the reference repo to use during clone
       (optional)
     :arg str scm-name: The unique scm name for this Git SCM (optional)
-    :arg bool wipe-workspace: Wipe out workspace before build (default true)
     :arg bool ignore-notify: Ignore notifyCommit URL accesses (default false)
     :arg str browser: what repository browser to use (default '(Auto)')
     :arg str browser-url: url for the repository browser (required if browser
@@ -127,6 +126,8 @@ remoteName/\*')
                     submodules operations (default: 10).
 
         :arg str timeout: Timeout for git commands in minutes (optional)
+        :arg bool wipe-workspace: Wipe out workspace before build
+          (default true)
 
     :browser values:
         :auto:
@@ -294,6 +295,11 @@ remoteName/\*')
                             'hudson.plugins.git.extensions.impl.'
                             'CheckoutOption')
         XML.SubElement(co, 'timeout').text = str(data['timeout'])
+    # By default we wipe the workspace
+    wipe_workspace = str(data.get('wipe-workspace', True)).lower()
+    if wipe_workspace == 'true':
+        ext_name = 'hudson.plugins.git.extensions.impl.WipeWorkspace'
+        ext = XML.SubElement(exts_node, ext_name)
 
     browser = data.get('browser', 'auto')
     browserdict = {'auto': 'auto',
