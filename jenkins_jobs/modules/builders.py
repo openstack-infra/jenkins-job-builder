74578358c8178a777b26a6e498b1b92bd23e7bff
Jaroslav Henner <jhenner@redhat.com>
2015-03-16 13:13:09 +0100

    Add checking of multijob phase condition.
    Incorrect setting of the `condition` leads to silent errors to update
    the job, so it is wise to check this in the JJB.
    
    Change-Id: Iae01b9f3d69c02f531995a57df12d813a45fda23

--- jenkins_jobs/modules/builders.py
+++ jenkins_jobs/modules/builders.py
@@ -1100,7 +1100,10 @@ def multijob(parser, xml_parent, data):
     :py:class:`jenkins_jobs.modules.project_multijob.MultiJob` projects.
 
     :arg str name: MultiJob phase name
-    :arg str condition: when to trigger the other job (default 'SUCCESSFUL')
+    :arg str condition: when to trigger the other job.
+        Can be: 'SUCCESSFUL', 'UNSTABLE', 'COMPLETED', 'FAILURE'.
+        (default 'SUCCESSFUL')
+
     :arg list projects: list of projects to include in the MultiJob phase
 
       :Project: * **name** (`str`) -- Project name
@@ -1134,6 +1137,10 @@ def multijob(parser, xml_parent, data):
     XML.SubElement(builder, 'phaseName').text = data['name']
 
     condition = data.get('condition', 'SUCCESSFUL')
+    conditions_available = ('SUCCESSFUL', 'UNSTABLE', 'COMPLETED', 'FAILURE')
+    if condition not in conditions_available:
+        raise JenkinsJobsException('Multijob condition must be one of: %s.'
+                                   % ', '.join(conditions_available))
     XML.SubElement(builder, 'continuationCondition').text = condition
 
     phaseJobs = XML.SubElement(builder, 'phaseJobs')
