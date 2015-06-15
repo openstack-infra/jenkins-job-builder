3cfa0481a0b25c06d457bf90b16d1731c32b799e
Jerome Hourquebie <jhourque@cisco.com>
2015-06-15 11:44:21 +0200

    Update xml-summary publisher
    Adding "Show on Project Page" option
    
    Change-Id: I15bc3b169b2c63b977aa7934744550f602893120

--- jenkins_jobs/modules/publishers.py
+++ jenkins_jobs/modules/publishers.py
@@ -2662,6 +2662,8 @@ def xml_summary(parser, xml_parent, data):
     <Summary+Display+Plugin>`.
 
     :arg str files: Files to parse (default '')
+    :arg bool shown-on-project-page: Display summary on project page
+        (default 'false')
 
     Example:
 
@@ -2673,6 +2675,8 @@ def xml_summary(parser, xml_parent, data):
                              'hudson.plugins.summary__report.'
                              'ACIPluginPublisher')
     XML.SubElement(summary, 'name').text = data['files']
+    XML.SubElement(summary, 'shownOnProjectPage').text = str(
+        data.get('shown-on-project-page', 'false'))
 
 
 def robot(parser, xml_parent, data):
