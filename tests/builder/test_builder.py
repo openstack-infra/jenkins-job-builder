f5851f412f033e3f438b97cf5030a4eeba539243
Wayne Warren <waynr+launchpad@sdf.org>
2016-05-22 23:56:03 -0700

    Use JJBConfig() class
    Prevent temptation for other contributors to somehow depend on an
    implementation detail of the builder.Jenkins.jobs property.
    
    Change-Id: If6477db1f5bfe8f31d099f21614e894834faf009

--- tests/builder/test_builder.py
+++ tests/builder/test_builder.py
@@ -26,17 +26,18 @@ from testtools import TestCase
 class TestCaseTestBuilder(LoggingFixture, TestCase):
     def setUp(self):
         super(TestCaseTestBuilder, self).setUp()
-        jjb_config = JJBConfig()
-        jjb_config.builder['plugins_info'] = ['plugin1', 'plugin2']
-        jjb_config.validate()
-        self.builder = jenkins_jobs.builder.JenkinsManager(jjb_config)
+        self.jjb_config = JJBConfig()
+        self.jjb_config.validate()
 
     def test_plugins_list(self):
+        self.jjb_config.builder['plugins_info'] = ['plugin1', 'plugin2']
+        self.builder = jenkins_jobs.builder.JenkinsManager(self.jjb_config)
         self.assertEqual(self.builder.plugins_list, ['plugin1', 'plugin2'])
 
     @mock.patch.object(jenkins_jobs.builder.jenkins.Jenkins,
                        'get_plugins_info', return_value=['p1', 'p2'])
     def test_plugins_list_from_jenkins(self, jenkins_mock):
         # Trigger fetching the plugins from jenkins when accessing the property
-        self.builder._plugins_list = None
+        self.jjb_config.builder['plugins_info'] = None
+        self.builder = jenkins_jobs.builder.JenkinsManager(self.jjb_config)
         self.assertEqual(self.builder.plugins_list, ['p1', 'p2'])
