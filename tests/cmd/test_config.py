652806a7513d017529774854965cf41ad60f6ef1
Darragh Bailey <dbailey@hpe.com>
2016-07-22 16:55:27 +0100

    Ensure flush cache option obeyed
    Make sure that flush cache is correct set first by config file, and
    additionally that the CLI overrides any defaults.
    
    In the V2 API work, when moving code around the flush cache option was
    accidentally removed from being set by the command line or config
    files. This ensures it follows the same behaviour as other similar
    options.
    
    Change-Id: I9fb1e234e5ed081ada64855389a87d2f7555469e

--- tests/cmd/test_config.py
+++ tests/cmd/test_config.py
@@ -87,6 +87,7 @@ class TestConfigs(CmdTestsBase):
         self.assertEqual(jjb_config.jenkins['user'], "jenkins_user")
         self.assertEqual(jjb_config.jenkins['password'], "jenkins_password")
         self.assertEqual(jjb_config.builder['ignore_cache'], True)
+        self.assertEqual(jjb_config.builder['flush_cache'], True)
         self.assertEqual(
             jjb_config.yamlparser['allow_empty_variables'], True)
 
@@ -96,12 +97,13 @@ class TestConfigs(CmdTestsBase):
         when non of the global CLI options are set.
         """
         args = ['--user', 'myuser', '--password', 'mypassword',
-                '--ignore-cache', '--allow-empty-variables',
+                '--ignore-cache', '--flush-cache', '--allow-empty-variables',
                 'test', 'dummy.yaml']
         jenkins_jobs = entry.JenkinsJobs(args)
         jjb_config = jenkins_jobs.jjb_config
         self.assertEqual(jjb_config.jenkins['user'], "myuser")
         self.assertEqual(jjb_config.jenkins['password'], "mypassword")
         self.assertEqual(jjb_config.builder['ignore_cache'], True)
+        self.assertEqual(jjb_config.builder['flush_cache'], True)
         self.assertEqual(
             jjb_config.yamlparser['allow_empty_variables'], True)
