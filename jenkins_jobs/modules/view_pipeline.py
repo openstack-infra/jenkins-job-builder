ac1f2606a585e6b6c4cecb6c66d143b665b1e483
Thanh Ha <zxiiro@linux.com>
2018-07-23 23:33:27 -0400

    [pipeline] Update helpers import to common style
    Change-Id: I7ceb2e3bf23e9c92f13a7b86b8361b2bf98dbd81
    Signed-off-by: Thanh Ha <zxiiro@linux.com>

--- jenkins_jobs/modules/view_pipeline.py
+++ jenkins_jobs/modules/view_pipeline.py
@@ -62,8 +62,7 @@ Example:
 
 import xml.etree.ElementTree as XML
 import jenkins_jobs.modules.base
-
-from jenkins_jobs.modules.helpers import convert_mapping_to_xml
+import jenkins_jobs.modules.helpers as helpers
 
 
 class Pipeline(jenkins_jobs.modules.base.Base):
@@ -78,8 +77,9 @@ class Pipeline(jenkins_jobs.modules.base.Base):
         mapping_optional = [
             ('description', 'description', None),
             ('filter-executors', 'filterExecutors', False),
-            ('filter-queue', 'filterQueue', False)]
-        convert_mapping_to_xml(root, data,
+            ('filter-queue', 'filterQueue', False),
+        ]
+        helpers.convert_mapping_to_xml(root, data,
             mapping_optional, fail_required=False)
 
         XML.SubElement(root, 'properties',
@@ -105,8 +105,8 @@ class Pipeline(jenkins_jobs.modules.base.Base):
                 'showPipelineParametersInHeaders', False),
             ('start-with-parameters', 'startsWithParameters', False),
             ('refresh-frequency', 'refreshFrequency', 3),
-            ('definition-header', 'showPipelineDefinitionHeader', False)
+            ('definition-header', 'showPipelineDefinitionHeader', False),
         ]
-        convert_mapping_to_xml(root, data, mapping, fail_required=True)
+        helpers.convert_mapping_to_xml(root, data, mapping, fail_required=True)
 
         return root
